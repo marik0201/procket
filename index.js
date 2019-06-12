d1442040f511a78d39e680640958885044af9983
marik0201 <31615728+marik0201@users.noreply.github.com>
2019-06-12 16:33:29 +0300

    code refactoring, add config

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
 class Procket {
-  async get(url, header) {
+  constructor() {
+    this.config = {
+      timeout: 10 * 1000,
+      header: {
+        "Content-type": "application/json"
+      }
+    };
+  }
+
+  async sendRequest(url, method, body, config) {
     return new Promise((resolve, reject) => {
+      let requestConfig = { ...this.config, ...config };
       const xhr = new XMLHttpRequest();
-      header && xhr.setRequestHeader(header);
-      xhr.open("GET", url, true);
+
+      xhr.open(method, url, true);
+
+      setTimeout(() => {
+        reject(new Error("Time is out"));
+      }, requestConfig.timeout);
+
+      for (const key in requestConfig.header) {
+        xhr.setRequestHeader(key, requestConfig.header[key]);
+      }
 
       xhr.onload = function() {
-        if (this.status.toString()[0] == "2") {
+        if (this.status >= 200 && this.status < 400) {
           resolve(JSON.parse(this.response));
         } else {
           const error = new Error(this.statusText);
@@ -19,107 +37,25 @@ class Procket {
         reject(new Error("Network Error"));
       };
 
-      xhr.send();
+      xhr.send(JSON.stringify(body));
     });
   }
 
-  async post(url, body, header) {
-    return new Promise((resolve, reject) => {
-      const xhr = new XMLHttpRequest();
-      header && xhr.setRequestHeader(header);
-      xhr.open("POST", url, true);
-
-      xhr.onload = function() {
-        if (this.status.toString()[0] == "2") {
-          resolve(JSON.parse(this.response));
-        } else {
-          const error = new Error(this.statusText);
-          error.code = this.status;
-          reject(error);
-        }
-      };
-
-      xhr.onerror = function() {
-        reject(new Error("Network Error"));
-      };
-
-      xhr.send(body);
-    });
+  async get(url, config) {
+    return this.sendRequest(url, "GET", config);
   }
 
-  async delete(url, body, header) {
-    return new Promise((resolve, reject) => {
-      const xhr = new XMLHttpRequest();
-      header && xhr.setRequestHeader(header);
-      xhr.open("DELETE", url, true);
-
-      xhr.onload = function() {
-        if (this.status.toString()[0] == "2") {
-          resolve(JSON.parse(this.response));
-        } else {
-          const error = new Error(this.statusText);
-          error.code = this.status;
-          reject(error);
-        }
-      };
-
-      xhr.onerror = function() {
-        reject(new Error("Network Error"));
-      };
-
-      xhr.send(body);
-    });
+  async post(url, body, config) {
+    return this.sendRequest(url, "POST", body, config);
   }
 
-  async put(url, body, header) {
-    return new Promise((resolve, reject) => {
-      const xhr = new XMLHttpRequest();
-      header && xhr.setRequestHeader(header);
-      xhr.open("PUT", url, true);
-
-      xhr.onload = function() {
-        if (this.status.toString()[0] == "2") {
-          resolve(JSON.parse(this.response));
-        } else {
-          const error = new Error(this.statusText);
-          error.code = this.status;
-          reject(error);
-        }
-      };
-
-      xhr.onerror = function() {
-        reject(new Error("Network Error"));
-      };
-
-      xhr.send(body);
-    });
+  async delete(url, body, config) {
+    return sendRequest(url, "DELETE", body, config);
   }
 
-  async patch(url, body, header) {
-    return new Promise((resolve, reject) => {
-      const xhr = new XMLHttpRequest();
-      header && xhr.setRequestHeader(header);
-      xhr.open("PATCH", url, true);
-
-      xhr.onload = function() {
-        if (this.status.toString()[0] == "2") {
-          resolve(JSON.parse(this.response));
-        } else {
-          const error = new Error(this.statusText);
-          error.code = this.status;
-          reject(error);
-        }
-      };
-
-      xhr.onerror = function() {
-        reject(new Error("Network Error"));
-      };
-
-      xhr.send(body);
-    });
+  async put(url, body, config) {
+    return sendRequest(url, "PUT", body, config);
   }
 }
 
-const procket = new Procket();
-
-module.exports = procket;
+module.exports = Procket;
